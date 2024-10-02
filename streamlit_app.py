fa8bc13ab918937daffb4f8be9a35475fb1086ba
Yash Pratap Gautam <76730315+yash-g01@users.noreply.github.com>
2024-10-02 04:58:13 +0000

    Pending changes exported from your codespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,48 @@
 import streamlit as st
-from openai import OpenAI
+import google.generativeai as genai
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+# Page Customisation
+st.set_page_config(
+   page_title="Cancer Diagnosis AI",
+   page_icon="🧑‍⚕️",
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# Show title and description.
+st.title("🧑‍⚕️ Cancer Diagnosis AI")
+st.write(
+    "Upload a document below or ask a question – Gen AI will answer! "
+    "To use this app, upload a document for cancer diagnosis or just ask anything related to cancer."
+)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# Gemini API
+genai.configure(api_key="AIzaSyDL4FArHVQaHu7ego_1188hyvZ6bZ018Uc")
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
-    )
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document"
+)
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+# Ask the user for a question via `st.text_area`.
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?"
+)
 
-    if uploaded_file and question:
+if uploaded_file and question:
+# Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+try:
+    # Generate an answer using the Gemini API.
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(question)
+    # Stream the response to the app using `st.write_stream`.
+    st.write(response.text)
+except TypeError:
+    pass
